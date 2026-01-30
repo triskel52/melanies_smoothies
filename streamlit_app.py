6b66bef90ec68a508c9ae7139c7a67d32ce12098
triskel52 <jean-francois.saluden@follow-us.fr>
2026-01-30 11:00:02 +0100

    Update streamlit_app.py
    Remove trace on search_on query

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-        st.write(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
